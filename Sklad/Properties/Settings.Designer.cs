6ce3c825fb1625ba6e62debce8bfa175c5006324
Dedulko Nikita <36064675+DedulkoN@users.noreply.github.com>
2025-04-20 08:40:20 +0300

    14

--- Sklad/Properties/Settings.Designer.cs
+++ Sklad/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Sklad.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,7 @@ namespace Sklad.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=127.0.0.1;Initial Catalog=Sklad;Persist Security Info=True;User ID=Ad" +
-            "min;Password=Е6роь*ГШ")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(Local);Initial Catalog=Sklad;Integrated Security=True;")]
         public string SkladConnectionString {
             get {
                 return ((string)(this["SkladConnectionString"]));
